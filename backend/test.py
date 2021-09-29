0edaf13c9d2823cb4676413cb6c164b59b55eae5
Zach McFadden <zach.mcfadden@workday.com>
2021-09-28 17:39:41 -0700

    fix test.py

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,7 @@
 from src.util.LogFactory import LogFactory
 from src.util.ErrorFactory import errorStackTrace
 from src.App import App
+from src.Setup import Setup
 
 from test.util.decorators.Toggle import TestMetrics
 from test.integration.IntegrationTests import IntegrationTests
@@ -9,7 +10,7 @@ from test.unit.UnitTests import UnitTests
 def test():
   try:
     LogFactory.log_level = 'DEBUG'
-    App.init_main_app_resources()
+    Setup.init_main_app_resources()
     LogFactory.MAIN_LOG.info('====== START TEST RUNNER ======')
     UnitTests.run_unit_tests()
     IntegrationTests.run_integration_tests()
