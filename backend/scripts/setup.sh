83b580a84f29e98067bfcad8357c6222db06dde7
Zach McFadden <zach.mcfadden@workday.com>
2021-09-29 10:29:35 -0700

    update setup script

--- backend/scripts/setup.sh
+++ backend/scripts/setup.sh
@@ -1,20 +1,23 @@
 #! /bin/sh
 
-#PYTHON_INTERPRETER=($(which python3 || which python))
+PYTHON_INTERPRETER=($(which python3 || which python))
 
-#if [[ $? != 0 ]];then
-#  echo "No python installation detected, trying to install.."
-#  if [[ $(uname -s) == 'Darwin']];then
-#    brew install python3
-#  else
-#    # Assume ubuntu container
-#    apt-get install python3
-#  fi
-#
-#fi
+if [[ $? != 0 ]];then
+ echo "No python installation detected, trying to install.."
+ if [[ $(uname -s) == 'Darwin' ]]; then
+   brew install python3
+ else
+   # Assume ubuntu container
+   apt-get install python3
+ fi
+fi
 
-python -m pip install pillow
-python -m pip install qrcode
-python -m pip install schedule
-python -m pip install redis
-python -m pip install psutil
+echo $PYTHON_INTERPRETER
+
+${PYTHON_INTERPRETER} -m pip install pillow
+${PYTHON_INTERPRETER} -m pip install qrcode
+${PYTHON_INTERPRETER} -m pip install schedule
+${PYTHON_INTERPRETER} -m pip install redis
+${PYTHON_INTERPRETER} -m pip install psutil
+${PYTHON_INTERPRETER} -m pip install flask
+${PYTHON_INTERPRETER} -m pip install flask-cors
