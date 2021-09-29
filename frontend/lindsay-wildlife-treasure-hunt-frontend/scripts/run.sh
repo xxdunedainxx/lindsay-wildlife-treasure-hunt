09d46493e39f8c96129294540d2eabfe7b7ad98a
Zach McFadden <zach.mcfadden@workday.com>
2021-09-29 11:57:37 -0700

    update runner

--- frontend/lindsay-wildlife-treasure-hunt-frontend/scripts/run.sh
+++ frontend/lindsay-wildlife-treasure-hunt-frontend/scripts/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 # HTTPS is required for the barcode scanner to be allowed in browser environments
-HTTPS=true npm start
+# Skip preflight check due to some testing framework library issues :(
+HTTPS=true SKIP_PREFLIGHT_CHECK=true npm start
