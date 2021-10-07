800349cf549382211c79a2b7e45525941e324240
Roderick MacLeod <roderickjmacleod@gmail.com>
2021-10-07 14:20:00 -0700

    added email addresses for testing

--- backend/test/unit/util/EmailValidationUnitTesting.py
+++ backend/test/unit/util/EmailValidationUnitTesting.py
@@ -9,14 +9,44 @@ import unittest
 GOOD_EMAILS: [str] = [
   "someone@aol.com",
   "someelse@gmail.com",
-  "somethingfsjkldajsgadhgladslfhdsafadsf@hotmail.com"
+  "somethingfsjkldajsgadhgladslfhdsafadsf@hotmail.com",
+  "email@example.com",
+  "firstname.lastname@example.com",
+  "email@subdomain.example.com",
+  "firstname+lastname@example.com",
+  "email@123.123.123.123",
+  "email@[123.123.123.123]",
+  "1234567890@example.com",
+  "email@example-one.com",
+  "_______@example.com",
+  "email@example.name",
+  "email@example.museum",
+  "email@example.co.jp",
+  "firstname-lastname@example.com"
 ]
 
 BAD_EMAILS: [str] = [
   "fdasfdasfas",
   "@fad@@@fadsfsadfasd",
   "@@@@@@",
-  ".com"
+  ".com",
+  "plainaddress",
+  "#@%^%#$@#$@#.com",
+  "@example.com",
+  "Joe Smith <email@example.com>",
+  "email.example.com",
+  "email@example@example.com",
+  ".email@example.com",
+  "email.@example.com",
+  "email..email@example.com",
+  "あいうえお@example.com",
+  "email@example.com (Joe Smith)",
+  "email@example",
+  "email@-example.com",
+  "email@example.web",
+  "email@111.222.333.44444",
+  "email@example..com",
+  "Abc..123@example.com"
 ]
 
 @enabled
