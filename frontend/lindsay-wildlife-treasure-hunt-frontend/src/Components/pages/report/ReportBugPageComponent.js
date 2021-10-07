39ac6f2e96eaa964fe4c77f06c4743f9052fd5ef
Zach McFadden <zach.mcfadden@workday.com>
2021-10-07 15:53:32 -0700

    nav + router

---
+++ frontend/lindsay-wildlife-treasure-hunt-frontend/src/Components/pages/report/ReportBugPageComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ReportBugPageComponent.css';
+
+export class ReportBugPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return (
+      <div>
+        report a bug
+      </div>
+    );
+  }
+}
+
+export default ReportBugPage;
