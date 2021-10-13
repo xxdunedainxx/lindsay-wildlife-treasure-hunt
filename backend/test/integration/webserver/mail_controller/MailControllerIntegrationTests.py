5d7e954e1dd85af7d58ed3063330d370fa88492f
Zach McFadden <zach.mcfadden@workday.com>
2021-10-13 14:58:32 -0700

    HTML Email formatter, email template example,update tests, controller etc

--- backend/test/integration/webserver/mail_controller/MailControllerIntegrationTests.py
+++ backend/test/integration/webserver/mail_controller/MailControllerIntegrationTests.py
@@ -21,7 +21,7 @@ class MailControllerIntegrationTests(unittest.TestCase):
       MailQ = Singletons.mailQ
       # get size before adding email
       q_size = MailQ.q_size()
-      dictionary_object = {"email": "test@google.com"}
+      dictionary_object = {"email": "zrmmaster92@gmail.com", "username": "Zach"}
       r = requests.post("http://localhost/mail",
           data=json.dumps(dictionary_object),
           headers={ "content-type": "application/json" }
@@ -29,7 +29,7 @@ class MailControllerIntegrationTests(unittest.TestCase):
       response_data = r.json()
       assert r.status_code == 200
       print (response_data["response"])
-      assert response_data["response"] == "valid email"
+      assert response_data["response"] == "Valid Request"
       redis_key = response_data["key"]
 
       assert (MailQ.q_size() == 1)
@@ -43,7 +43,7 @@ class MailControllerIntegrationTests(unittest.TestCase):
     @enabled
     def test_integration_sending_email_request_invalid_email(self):
       MailQ = Singletons.mailQ
-      dictionary_object = {"email": "badEmail"}
+      dictionary_object = {"email": "badEmail", "username": "Zach"}
       r = requests.post("http://localhost/mail",
           data=json.dumps(dictionary_object),
           headers={ "content-type": "application/json" }
@@ -51,9 +51,25 @@ class MailControllerIntegrationTests(unittest.TestCase):
 
       response_data = r.json()
       print (response_data["response"])
-      assert response_data["response"] == "invalid email"
+      assert response_data["response"] == "Invalid Request"
       assert r.status_code == 400
       assert (MailQ.q_size() == 0)
+
+    @enabled
+    def test_integration_sending_email_request_invalid_request(self):
+      MailQ = Singletons.mailQ
+      dictionary_object = {"email": "zrmmaster92@gmail.com"}
+      r = requests.post("http://localhost/mail",
+          data=json.dumps(dictionary_object),
+          headers={ "content-type": "application/json" }
+      )
+
+      response_data = r.json()
+      print (response_data["response"])
+      assert response_data["response"] == "no email provided, or no username provided"
+      assert r.status_code == 400
+      assert (MailQ.q_size() == 0)
+
 
     @enabled
     def test_integration_sending_email_request_no_payload(self):
