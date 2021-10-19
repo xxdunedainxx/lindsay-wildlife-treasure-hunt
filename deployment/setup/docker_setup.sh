d7ebdd21bcb9ca8c8ce6968c00b4e2d7fb726e0c
Zach McFadden <zach.mcfadden@workday.com>
2021-10-19 15:53:30 -0700

    deployment scripts

---
+++ deployment/setup/docker_setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+function setupDocker(){
+  sudo amazon-linux-extras install docker
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+  sudo newgrp docker
+  sudo chkconfig docker on
+}
+
+function setupDockerCompose(){
+  sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  docker-compose version
+}
+
+setupDocker
+setupDockerCompose
