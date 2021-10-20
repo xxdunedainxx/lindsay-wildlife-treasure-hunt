9d80afc11af6b08531229b82d93493b6e588173a
Zach McFadden <zach.mcfadden@workday.com>
2021-10-20 12:22:07 -0700

    local things

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,13 +2,17 @@
 
 HOME=$(pwd)
 
-cd frontend
+cd lindsay-wildlife-treasure-hunt-frontend
 
 rm -rf docker/tmp
 mkdir docker/tmp
 
+./scripts/build.sh
+
+cd $HOME
+
+cp -r lindsay-wildlife-treasure-hunt-frontend/build/ docker/tmp
+
 cd docker 
 docker build . -f Dockerfile.ReactApp -t lindsay-react-app
 cd $HOME 
-
-cp -r lindsay-wildlife-treasure-hunt-frontend/build/ docker/tmp
