818dae18c1fff97a49282b1d518c19571b7ab767
Zach McFadden <zach.mcfadden@workday.com>
2021-10-25 12:18:52 -0700

    fix gamepage component issue

--- frontend/lindsay-wildlife-treasure-hunt-frontend/src/Components/pages/game/GamePageComponent.js
+++ frontend/lindsay-wildlife-treasure-hunt-frontend/src/Components/pages/game/GamePageComponent.js
@@ -4,19 +4,60 @@ import './GamePageComponent.css';
 import {PageBody} from '../../PageBody/PageBody';
 import {GameDisplay} from '../../GameDisplay/GameDisplay';
 
+import GameControllerClient from '../../../src/http/clients/GameControllerClient';
+import Configuration from '../../../src/conf/Configuration';
+
 export class GamePage extends React.Component {
+  static gamePageInstance = null
+
+  enablePage(){
+    this.setState(
+      {
+        gameStateLoaded: true
+      }
+    );
+  }
+
+  async fetchGameState(){
+    var gameControllerClient = new GameControllerClient(Configuration.remoteEndpoint)
+    
+    // ensures callback to this page to toggle it on
+    GameControllerClient.dependentPage=this;
+    
+    await gameControllerClient.getDb()
+  }
+
+
+  //after the component is mounted, fetch game state data
+  componentDidMount(){
+    this.fetchGameState()
+  }
 
   constructor(props) {
     super(props)
+    GamePage.gamePageInstance = this;
+    this.state = {
+      gameStateLoaded: false
+    }
   }
 
   render(){
-    return (
-      <div>
-         <PageBody />
-        <GameDisplay />
-      </div>
-    );
+    // if state is loaded, return gamedisplay, otherwise display loading message
+    if(this.state.gameStateLoaded) {
+      return (
+        <div>
+           <PageBody />
+          <GameDisplay />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <PageBody />
+          Loading...
+        </div>
+      );
+    }
   }
 }
 
