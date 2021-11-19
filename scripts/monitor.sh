7241c70c15ac67816f7bcbfc35018771f655355c
rjmacleod <roderickjmacleod@gmail.com>
2021-11-19 10:42:52 -0800

    Remove config.sh, setup.sh creates lindsay_monitor.src and prompts for values, enhance confirmation of cron job.

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-source config.sh
+source ~/.lindsay_monitor.src
+
+EMAIL_ARRAY=($EMAIL_ADDRESSES)
 
 # function to send email
 send_alert_email() {
-    to=$1
-    echo "Sending alert message to $to"
-    curl --ssl-reqd \
-  --url 'smtps://smtp.gmail.com:465' \
+    # TODO: function takes a space separated list of emails as args
+    #to=$1
+    #echo "Sending alert message to $to"
+    #curl --ssl-reqd \
+  #--url 'smtps://smtp.gmail.com:465' \
   # email password goes after colon on next line
-  --user 'lindsaywildlifetech@gmail.com:' \
-  --mail-from 'lindsaywildlifetech@gmail.com' \
-  --mail-rcpt '$to' \
-  --upload-file alert_email.txt
+  #--user 'lindsaywildlifetech@gmail.com:' \
+  #--mail-from 'lindsaywildlifetech@gmail.com' \
+  #--mail-rcpt '$to' \
+  #--upload-file alert_email.txt
 }
 
 # get app health status
@@ -25,11 +28,10 @@ else
     current_time_seconds=$(date +"%s")
     last_alert_seconds=$(cat ./scripts/LAST_ALERT.txt)
     # if it's been more than 30 mins since last alert, send alert emails
-    if [ $(($current_time_seconds - $last_alert_seconds)) -ge $ALERT_INTERVAL ]; then
+    if [ $(($current_time_seconds - $last_alert_seconds)) -ge $ALERT_INTERVAL_SECONDS ]; then
         minutes_since_alert=$(expr $(($current_time_seconds - $last_alert_seconds)) / 60)
         echo $minutes_since_alert' minutes since last alert'
-        send_alert_email $ZACH_EMAIL
-        send_alert_email $RORY_EMAIL
+        # send_alert_email
         # rewrite LAST_ALERT.txt with current time
         > LAST_ALERT.txt
         echo "${current_time_seconds}" > "LAST_ALERT.txt"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-source config.sh
-
 # install jq
 apt install jq -y
 
+# Create monitor_source file
+touch ~/.lindsay_monitor.src
+read -p "please provide the lindsay app url" LINDSAY_URL_INPUT
+echo "export LINDSAY_URL=${LINDSAY_URL_INPUT}" >> ~/.lindsay_monitor.src
+read -p "please provide the email address(es) to be alerted, space separated" EMAIL_ADDRESSES
+echo "export EMAILS_TO_ALERT=${EMAIL_ADDRESSES}" >> ~/.lindsay_monitor.src
+read -p "How many seconds between alerts? (recommended 1800s=30min)" SECONDS
+echo "export ALERT_INTERVAL_SECONDS=$($SECONDS)" >> ~/.lindsay_monitor.src
+
+# ==========
+#  Cron job
+# ==========
+
 HOME=$(pwd)
+DIR=$(pwd)
+MONITOR_JOB=$(cat ./scripts/MONITOR_JOB.txt)
 MONITOR_SCRIPT=($HOME/monitor.sh)
 CRON_FILE="/var/spool/cron/root"
 
@@ -26,8 +39,10 @@ echo "Scheduling cron job for monitoring app..."
 # confirm job was scheduled
 # (this is kind of a hack, but it works I think)
 CURRENT_JOBS=$(crontab -l)
-if [[ "${CURRENT_JOBS}" == "${MONITOR_JOB} $DIR/monitor.sh" ]]; then
+if [[ "${CURRENT_JOBS}" == *"${MONITOR_JOB} $DIR/monitor.sh"* ]]; then
       echo "Cron job scheduled!"
+	  exit 0
 else
       echo "WARNING: Cron job not set."
+	  exit 1
 fi
