b1b326a43cc8396591241fa5329af1aa39a0e658
Zach McFadden <zach.mcfadden@workday.com>
2021-11-30 15:41:04 -0800

    Version 0.13.3

--- frontend/lindsay-wildlife-treasure-hunt-frontend/src/src/conf/Configuration.js
+++ frontend/lindsay-wildlife-treasure-hunt-frontend/src/src/conf/Configuration.js
@@ -10,7 +10,7 @@ export class Configuration {
 
   static Init() {
     console.log(confData)
-    Configuration.version = "0.13.2"
+    Configuration.version = "0.13.3"
     Configuration.logLevel = confData.logging.level
     Configuration.conf = confData
     Configuration.headers = confData.headers
