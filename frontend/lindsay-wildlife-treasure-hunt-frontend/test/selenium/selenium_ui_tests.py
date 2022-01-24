d157ba50e6d9ef17a6dc98f24034c848f7b7d516
rjmacleod <roderickjmacleod@gmail.com>
2022-01-24 11:41:29 -0800

    Update chrome driver to latest version

--- frontend/lindsay-wildlife-treasure-hunt-frontend/test/selenium/selenium_ui_tests.py
+++ frontend/lindsay-wildlife-treasure-hunt-frontend/test/selenium/selenium_ui_tests.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path="./drivers/chrome/chromedriver_macos")
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+# s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(executable_path="./drivers/chrome/chromedriver_linux")
+driver.get("https://lindsay-wildlife-apps.zee-aws.net/ui/home")
+elem = driver.find_element(By.CLASS_NAME, "welcomeMessage").text
+assert elem == "Welcome to our Biodiversity Scavenger Hunt!"
+
+driver.quit()
+
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
