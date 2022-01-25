afa2a5efdd3211273503d1e19c374e6b15edc312
rjmacleod <roderickjmacleod@gmail.com>
2022-01-24 16:47:44 -0800

    Fix typo in Configuration.py, rename ImageFetcher to ImageController

--- backend/src/Configuration.py
+++ backend/src/Configuration.py
@@ -33,7 +33,7 @@ class Configuration:
     "LOG_ROTATION_JOB_ARCHIVE_DIR"      : f"archive{os.sep}",
     "UI_LOGGING_JOB_INTERVAL_MINUTES" : 1,
     "UI_LOGGING_JOB_LOGS_PER_JOB"     : 50,
-    "BUG_REPORT_EMAIL_LIST" : ["zrmmaster92@gmail.com", "roderickjmacleod@gmail.com"]
+    "BUG_REPORT_EMAIL_LIST" : ["zrmmaster92@gmail.com", "roderickjmacleod@gmail.com"],
 
     # Static image directory
     "IMAGE_DIR" : f"{os.getcwd()}{os.sep}assets"
@@ -80,7 +80,7 @@ class Configuration:
     self.UI_LOGGING_JOB_INTERVAL_MINUTES: int = self.__get_value("UI_LOGGING_JOB_INTERVAL_MINUTES")
     self.UI_LOGGING_JOB_LOGS_PER_JOB: int = self.__get_value("UI_LOGGING_JOB_LOGS_PER_JOB")
     self.BUG_REPORT_EMAIL_LIST: [str] = self.__get_value("BUG_REPORT_EMAIL_LIST")
-    
+
     # Static images
     self.IMAGE_DIR: str = self.__get_value("IMAGE_DIR")
 
