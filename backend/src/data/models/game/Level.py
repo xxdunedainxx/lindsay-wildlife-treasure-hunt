260b2a6ae5557625e5bb1ef85ef09996e44f0121
Roderick MacLeod <roderickjmacleod@gmail.com>
2022-01-27 15:42:35 -0800

    Artifact Database Update(#194)
    * Artifact DB update
    * update how client pulls images
    * image controller 
    Co-authored-by: Zach McFadden <zach.mcfadden@workday.com>

--- backend/src/data/models/game/Level.py
+++ backend/src/data/models/game/Level.py
@@ -6,10 +6,12 @@ class Level(Model):
 
   def json_to_model(self, jObject: dict):
     self.name: str = self.get_attribute(jObject, "ArtifactName")
+    self.artifactId: str = self.get_attribute(jObject, "ArtifactId")
     self.clue: str = self.get_attribute(jObject, "Clue")
     self.hint: str = self.get_attribute(jObject, "AdditionalHint")
     self.correctMessage: str = self.get_attribute(jObject, "CorrectMessage")
     self.mediaLink: str = self.get_attribute(jObject, "MediaLink")
+    self.photoCredit: str = self.get_attribute(jObject, "PhotoCredit")
 
   def to_dictionary(self) -> dict:
     return self._original_object
