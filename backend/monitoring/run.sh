ac1084e05a7cb4f441b6b7777132916163233fff
Zach McFadden <zach.mcfadden@workday.com>
2022-01-27 15:40:09 -0800

    remove old monitoring scripts + update run.sh

--- backend/monitoring/run.sh
+++ backend/monitoring/run.sh
@@ -2,4 +2,6 @@
 
 echo "START MONITORING AGENT CONTAINER"
 
+docker stop lindsay_app_monitoring && docker rm lindsay_app_monitoring || echo "No pre-existing monitoring app"
+
 docker run -e "CONF_FILE=$(cat ./conf.json)" -e "MON_CONF=$(cat ./monitoring.conf.json)" --name lindsay_app_monitoring -d lindsay_app_monitoring
