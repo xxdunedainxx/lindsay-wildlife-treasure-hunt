83c23ac75105a219291e8bbaba0dfffe8a89c8ef
Zach McFadden <zach.mcfadden@workday.com>
2022-05-16 20:42:19 -0700

    login controller integration tests

--- backend/test/integration/IntegrationTests.py
+++ backend/test/integration/IntegrationTests.py
@@ -6,6 +6,8 @@ from test.integration.threading.WorkerIntegrationTests import threading_integrat
 from test.integration.webserver.monitoring.AppHealthMonitorIntegrationTests import app_health_controller_integration_tests
 from test.integration.webserver.scavenger_hunt.ScavengerHuntControllerIntegrationTests import scavenger_hunt_controller_tests
 from test.integration.asset_testing.AssetIntegrationTesting import asset_availability_testing
+from test.integration.webserver.login.LoginIntegrationTests import login_controller_integration_tests
+from test.integration.webserver.admin.TestJwtTokenIntegrationTests import test_jwt_token_tests
 
 class IntegrationTests:
 
@@ -19,3 +21,5 @@ class IntegrationTests:
     threading_integration_tests()
     scavenger_hunt_controller_tests()
     asset_availability_testing()
+    login_controller_integration_tests()
+    test_jwt_token_tests()
---
+++ backend/test/integration/webserver/login/LoginIntegrationTests.py
@@ -0,0 +1,63 @@
+from src.util.LogFactory import LogFactory
+from src.Configuration import Configuration, CONF_INSTANCE
+
+from test.util.decorators.Toggle import enabled, disabled
+
+import requests
+import unittest
+import json
+
+@enabled
+def login_controller_integration_tests():
+
+    LogFactory.MAIN_LOG.info(f"RUNNING ALL LOGIN INTEGRATION TESTS")
+    unittest.main(module=__name__, exit=False)
+
+class LoginControllerIntegrationTests(unittest.TestCase):
+
+    @enabled
+    def test_bad_logins(self):
+      invalid_username = {
+        "username": "bad",
+        "password": "bad"
+      }
+      r = requests.post(f"http://localhost:{CONF_INSTANCE.FLASK_PORT_BIND}/login",
+          headers={ "content-type": "application/json" },
+          data=json.dumps(invalid_username)
+      )
+      response_data = r.json()
+
+      assert response_data["message"] == 'unauthorized' and r.status_code == 401
+
+      badreq = {
+        "s": "bad",
+        "s2": "bad"
+      }
+
+
+      r = requests.post(f"http://localhost:{CONF_INSTANCE.FLASK_PORT_BIND}/login",
+          headers={ "content-type": "application/json" },
+          data=json.dumps(badreq)
+      )
+      response_data = r.json()
+
+      assert response_data["message"] == 'bad request' and r.status_code == 400
+
+    @enabled
+    def test_good_login(self):
+      uname: str = list(CONF_INSTANCE.ADMIN_USERS.keys())[0]
+      pw: str = CONF_INSTANCE.ADMIN_USERS[uname]
+
+      valid_username = {
+        "username": uname,
+        "password": pw
+      }
+      r = requests.post(f"http://localhost:{CONF_INSTANCE.FLASK_PORT_BIND}/login",
+          headers={ "content-type": "application/json" },
+          data=json.dumps(valid_username)
+      )
+      response_data = r.json()
+
+      assert response_data["message"] == 'authorized' and r.status_code == 200 and 'jwt' in response_data.keys()
+if __name__ == "__main__":
+    unittest.main()
